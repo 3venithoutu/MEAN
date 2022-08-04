503f167711c1916f239d9fc08b5564e89e3fcd80
Triveni Thoutu <triveni.thoutu@gmail.com>
2022-08-04 16:26:38 +0530

    First commit

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: '<app-post-create>',
+  templateUrl: './post-create.component.html',
+  styleUrls: []
+})
+
+export class PostCreateComponent implements OnInit{
+  ngOnInit() {
+
+  }
+}
