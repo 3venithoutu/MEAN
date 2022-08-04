76aab898b423ec434411db6774756a08e0d1a2e1
Triveni Thoutu <triveni.thoutu@gmail.com>
2022-08-04 17:08:25 +0530

    adding material button, input, card and toolbar

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -3,11 +3,17 @@ import { Component, OnInit } from "@angular/core";
 @Component({
   selector: '<app-post-create>',
   templateUrl: './post-create.component.html',
-  styleUrls: []
+  styleUrls: ['./post-create.component.css']
 })
 
 export class PostCreateComponent implements OnInit{
+  enteredValue?: string;
   ngOnInit() {
 
   }
+
+  addPost(){
+    console.log(this.enteredValue);
+  }
+
 }
