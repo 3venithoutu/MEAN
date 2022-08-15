7027fab1bd4d8de128d835addff379bd427430d7
Triveni Thoutu <triveni.thoutu@gmail.com>
2022-08-15 15:57:27 +0530

    Adding data through node api

--- Backend/app.js
+++ Backend/app.js
@@ -1,18 +1,30 @@
 const express = require("express");
-
+const bodyParser = require("body-parser");
 const app = express();
 
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded)
 // app.use((req, res, next) => {
 //   console.log("first middleware");
 //   next();
 // });
 
+
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Origin-Headers", "Origin, X-Requested-With, Content-Type, Access");
-  res.setHeader("Access-Control-Origin-Method", "GET, POST, PATCH, DELETE, PUT, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Access");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, PUT, OPTIONS");
   next();
 })
+
+app.post("/api/posts", (req, res, next) => {
+  const post = req.body;
+  console.log(post);
+  res.status(201).json({
+    message: "Post added successfully"
+  });
+})
+
 
 app.use('/api/posts', (req, res, next) => {
   const posts = [
@@ -27,8 +39,7 @@ app.use('/api/posts', (req, res, next) => {
       content: "This is coming from server!"
     }
   ];
-  console.log("Hello");
-  res.status('200').json(
+  res.status(200).json(
     {
       message: "Posts fetched successfully!",
       posts: posts
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -27,8 +27,11 @@ export class PostsService{
 
   addPost(title: string, content: string){
     const post = { id: 'null', title: title, content: content};
-    this.posts.push(post);
-    this.postUpdated.next([...this.posts]);
+    this.http.post<{message: string}>('http://localhost:3000/api/posts', post).subscribe((response) => {
+      console.log(response);
+      this.posts.push(post);
+      this.postUpdated.next([...this.posts]);
+    });
   }
 
 }
